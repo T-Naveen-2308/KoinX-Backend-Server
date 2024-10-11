c30414238e2750e95f84a0ea5d1d055d5257f83c
T-Naveen-2308 <naveentummala033@gmail.com>
2024-10-11 10:20:20 +0530

    Created /stats endpoint

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,3 +1,22 @@
 import { PrismaClient } from "@prisma/client";
+import { round } from "../utils/utils.js";
 
 const prisma = new PrismaClient();
+
+// GET https://localhost:4000/api/stats?coin=
+async function getStatistics(req, res) {
+    let { coin } = req.query;
+    if (coin === "matic-network") {
+        coin = "matic_network";
+    }
+    const coinData = await prisma.coin.findFirst({
+        where: { coinType: coin },
+        orderBy: { createdTime: "desc" },
+        select: { price: true, marketCap: true, change24Hour: true }
+    });
+    return res.status(200).send({
+        price: round(coinData.price, 2),
+        marketCap: round(coinData.marketCap, 2),
+        "24hChange": round(coinData.change24Hour, 2)
+    });
+}
--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
+import expressAsyncHandler from "express-async-handler";
+import { getStatistics, getDeviation } from "../controllers/controllers.js";
 
 const mainApp = Router();
+
+mainApp.get("/stats", expressAsyncHandler(getStatistics));
 
 export default mainApp;
